81bb26cfa409081a65d5dacd9b55b890cfb6ac36
Hector Torrez <torrezhectorb@gmail.com>
2024-05-28 20:57:15 -0600

    chore: Update Supabase client configuration

--- src/db/api/server.ts
+++ src/db/api/server.ts
@@ -2,9 +2,13 @@ import type {Database} from "../types";
 
 import {createClient} from "@supabase/supabase-js";
 
+// const supabase = createClient<Database>(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.SUPABASE_SERVICE_ROLE!,
+// );
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE!,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY!,
 );
 
 export default supabase;

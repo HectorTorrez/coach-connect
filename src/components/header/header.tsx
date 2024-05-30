28aeeee903dea469ec2cd85c035f03beaca4f2a9
Hector Torrez <torrezhectorb@gmail.com>
2024-05-30 15:28:03 -0600

    Chore: Setup clerk webhooks

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,53 +1,18 @@
 "use client";
 
 import {usePathname} from "next/navigation";
+import {SignIn, SignInButton, UserButton, useUser} from "@clerk/nextjs";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
-import {Button} from "../ui/button";
 import {SearchClients} from "../clients/search";
 
 export function Header() {
   const pathname = usePathname();
+  const {user, isSignedIn} = useUser();
 
   return (
     <header className="flex h-[60px] items-center gap-4 border-b  px-3 ">
       <div className="w-full flex-1">{pathname === "/clients" && <SearchClients />}</div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            className="h-8 w-8 rounded-full border border-gray-200 dark:border-gray-800"
-            size="icon"
-            variant="ghost"
-          >
-            <img
-              alt="Avatar"
-              className="rounded-full"
-              height="32"
-              style={{
-                aspectRatio: "32/32",
-                objectFit: "cover",
-              }}
-              width="32"
-            />
-            <span className="sr-only">Toggle user menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      {isSignedIn ? <UserButton /> : <SignInButton />}
     </header>
   );
 }
